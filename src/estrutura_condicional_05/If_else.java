c389b338ab65f52c814fa9fb7fe8bc5c387bb1c2
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-07-24 09:25:26 -0300

    chore: add if_else

--- src/estrutura_condicional_05/If_else.java
+++ src/estrutura_condicional_05/If_else.java
@@ -3,26 +3,27 @@ package estrutura_condicional_05;
 import java.util.Scanner;
 
 public class If_else {
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		/* O usuário deseja receber uma mensagem de bom dia, boa tarde ou boa noite
-		 * Se for de manhã, bom dia, se a tarde, boa tarde e se de noite boa noite
+
+		/*
+		 * O usuário deseja receber uma mensagem de bom dia, boa tarde ou boa noite Se
+		 * for de manhã, bom dia, se a tarde, boa tarde e se de noite boa noite
 		 */
 		System.out.print("Que horas são? -> ");
 		double hours = sc.nextDouble();
-		
+
 		String msg;
 		if (hours >= 5.0 && hours < 12.0) {
 			msg = "Bom dia!";
-		}else if(hours >= 12.0 && hours < 18.0) {
+		} else if (hours >= 12.0 && hours < 18.0) {
 			msg = "Boa tarde!";
-		}else {
+		} else {
 			msg = "Boa noite!";
 		}
-		
+
 		System.out.println(msg);
-		
+
 		sc.close();
 	}
 }
