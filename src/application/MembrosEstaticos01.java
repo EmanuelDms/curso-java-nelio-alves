a2ebf221920d64e405ff659e046dfc81f37db0d1
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-08-13 07:48:33 -0300

    feat: membros estáticos (versão 3)

--- src/application/MembrosEstaticos01.java
+++ src/application/MembrosEstaticos01.java
@@ -8,20 +8,19 @@ import util.Calculator;
 public class MembrosEstaticos01 {
 
 	public static void main(String[] args) {
-		Calculator calc = new Calculator();
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 
-		double c = calc.circumference(radius);
+		double c = Calculator.circumference(radius);
 
-		double v = calc.volume(radius);
+		double v = Calculator.volume(radius);
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI: %.2f%n", calc.PI);
+		System.out.printf("PI: %.2f%n", Calculator.PI);
 
 		sc.close();
 	}
