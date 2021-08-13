2a63c80360b45a53737277ada548ab3d27540163
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-08-13 09:14:03 -0300

    feat: construtores

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		System.out.println("* Enter product data:");
+		
+		System.out.print("Name -> ");
+		String name = sc.nextLine();
+
+		System.out.print("Price -> ");
+		double price = sc.nextDouble();
+
+		System.out.print("Quantity in stock -> ");
+		int quantity = sc.nextInt();
+		
+		Product product = new Product(name, price, quantity);
+
+		System.out.println("* Product data: " + product);
+
+		System.out.print("Enter the number of products to be " + "added in stock: ");
+		product.AddProducts(sc.nextInt());
+
+		System.out.println("* Updated Product data: " + product);
+
+		System.out.print("Enter the number of products to be " + "removed from stock: ");
+		product.RemoveProducts(sc.nextInt());
+
+		sc.close();
+
+		System.out.println("* Updated Product data: " + product);
+	}
+
+}
