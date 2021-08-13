6fa577b94467e944841d9f22bab96dc9212e626e
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-08-13 13:19:35 -0300

    feat: encapsulamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,9 @@ public class Program {
 		int quantity = sc.nextInt(); */
 		
 		Product product = new Product(name, price/* , quantity */);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
 
 		System.out.println("* Product data: " + product);
 
@@ -37,6 +40,10 @@ public class Program {
 		sc.close();
 
 		System.out.println("* Updated Product data: " + product);
+		
+		System.out.println(product.getName());
+		System.out.println(product.getPrice());
+		System.out.println(product.getQuantity());
 	}
 
 }
