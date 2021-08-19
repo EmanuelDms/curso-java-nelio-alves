bb608b2206296e4b285032eaa1a447e8da6eb4c0
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-08-19 18:15:48 -0300

    feat: vetores parte 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,48 +3,33 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
-
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-		System.out.println("* Enter product data:");
+		int n = sc.nextInt();
+		double[] vect = new double[n];
 
-		System.out.print("Name -> ");
-		String name = sc.nextLine();
-
-		System.out.print("Price -> ");
-		double price = sc.nextDouble();
-
-		/*
-		 * System.out.print("Quantity in stock -> "); int quantity = sc.nextInt();
-		 */
-
-		Product product = new Product(name, price/* , quantity */);
-
-		product.setName("Computer");
-		System.out.println("Updated name: " + product.getName());
-
-		System.out.println("* Product data: " + product);
-
-		System.out.print("Enter the number of products to be " + "added in stock: ");
-		product.AddProducts(sc.nextInt());
-
-		System.out.println("* Updated Product data: " + product);
-
-		System.out.print("Enter the number of products to be " + "removed from stock: ");
-		product.RemoveProducts(sc.nextInt());
+		avgHeight(vect, sc);
 
 		sc.close();
+	}
 
-		System.out.println("* Updated Product data: " + product);
+	public static void avgHeight(double[] arr, Scanner sc) {
+		double sum = 0.0;
+		int i = 0;
 
-		System.out.println(product.getName());
-		System.out.println(product.getPrice());
-		System.out.println(product.getQuantity());
+		while (i < arr.length) {
+			arr[i] = sc.nextDouble();
+			sum += arr[i];
+			i++;
+		}
+
+		double avg = sum / i;
+
+		System.out.printf("AVERAGE HEIGHT = %.2f", avg);
 	}
 
 }
